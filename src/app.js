9360253d24ec1288220334044048a462572beff6
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-10-17 15:22:47 +0000

    commit 1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "their", "my"];
+  let adjetive = ["great", "big", "strange", "silly"];
+  let nouns = ["jogger", "racoon", "cat", "cow"];
+  let extension = [".com", ".net", ".org", ".com.ve"];
+
+  for (let pron in pronoun) {
+    for (let adj in adjetive) {
+      for (let noun in nouns) {
+        for (let ext in extension) {
+          let domain = pron + adj + noun + ext;
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
